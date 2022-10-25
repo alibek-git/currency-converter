6c78a6f1d2b52fb416b73ba63ab8515a37798bd2
Alibek <83647897+alibek-git@users.noreply.github.com>
2022-10-25 15:59:00 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN = '1923148599:AAG2B5JTFG06mIC-8xPsfwm5FmqkRxM4FOc'  # Константа пишется в ALL CAPS
+TOKEN = '1923148599:AAG2B5JTFG06mIC-8xPsfwm5FmqkRxM4FOc' # Never upload Token to public repos! This project is for practice purposes only!
 
 
 keys = {'доллар': 'USD', 'евро': 'EUR', 'рубль': 'RUB', 'йена': 'JPY',
